d1fcc903b6f6fdebe30e470a13d4695324ee6207
Steve Tipton <slaatipton26@gmail.com>
2018-10-04 17:47:55 -0400

    Add initial UsersController

---
+++ scrimp/Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using scrimp.Domain;
+using System;
+
+namespace scrimp.Controllers
+{
+    // TODO add Authorization
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        // TODO implement DI
+        public UsersController() { }
+
+        // TODO consider authenticate/authorize/register endpoints
+
+        // GET users
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        // GET users/:id
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // PUT users/:id
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody]UserDto userDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        // DELETE users/:id
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ scrimp/Domain/UserDto.cs
@@ -0,0 +1,13 @@
+﻿namespace scrimp.Domain
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Timezone { get; set; }
+        public Weekday WeekStartDay { get; set; }
+        public string CurrencyCode { get; set; }
+    }
+}
