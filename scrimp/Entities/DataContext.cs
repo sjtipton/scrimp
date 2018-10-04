ac7a218e705e0f8c59587b75db19ca41c28fa5ff
Steve Tipton <slaatipton26@gmail.com>
2018-10-04 17:07:32 -0400

    Add DataContext and setup initial DI

---
+++ scrimp/Entities/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace scrimp.Entities
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<TransactionAccount> TransactionAccounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
