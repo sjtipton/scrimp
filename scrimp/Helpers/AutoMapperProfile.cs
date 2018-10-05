7a3cb901dd46b2741c4aedfbb7826814080d1072
Steve Tipton <slaatipton26@gmail.com>
2018-10-05 14:51:50 -0400

    Add Helpers

---
+++ scrimp/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using scrimp.Dtos;
+using scrimp.Entities;
+
+namespace scrimp.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+        }
+    }
+}
