b8c5ccb95866563f1e3a407894663780cf79c645
Steve Tipton <slaatipton26@gmail.com>
2018-10-08 11:56:53 -0400

    Add migrations and update EF

---
+++ scrimp/Migrations/20181008155154_UpdateSchemaAndForeignKeys.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace scrimp.Migrations
+{
+    public partial class UpdateSchemaAndForeignKeys : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_TransactionAccounts_TransactionAccountId",
+                table: "Transactions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TransactionAccountId",
+                table: "Transactions",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ClosingBalance",
+                table: "Transactions",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "Transactions",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AddColumn<int>(
+                name: "AccountId",
+                table: "Transactions",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Transactions",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "StartingBalance",
+                table: "TransactionAccounts",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CurrentBalance",
+                table: "TransactionAccounts",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "TransactionAccounts",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Categories",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CurrentBalance",
+                table: "Accounts",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Accounts",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_TransactionAccounts_TransactionAccountId",
+                table: "Transactions",
+                column: "TransactionAccountId",
+                principalTable: "TransactionAccounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_TransactionAccounts_TransactionAccountId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "AccountId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "TransactionAccounts");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Accounts");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TransactionAccountId",
+                table: "Transactions",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ClosingBalance",
+                table: "Transactions",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "Transactions",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "StartingBalance",
+                table: "TransactionAccounts",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CurrentBalance",
+                table: "TransactionAccounts",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CurrentBalance",
+                table: "Accounts",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_TransactionAccounts_TransactionAccountId",
+                table: "Transactions",
+                column: "TransactionAccountId",
+                principalTable: "TransactionAccounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
