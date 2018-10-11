6da787c66b425b82dcc17d3fb319bb5628747fff
Steve Tipton <slaatipton26@gmail.com>
2018-10-11 13:50:37 -0400

    Fix Migrations for Errors

---
+++ scrimp/Migrations/20181011174902_AddErrors.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace scrimp.Migrations
+{
+    public partial class AddErrors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppException",
+                columns: table => new
+                {
+                    HelpLink = table.Column<string>(nullable: true),
+                    Source = table.Column<string>(nullable: true),
+                    HResult = table.Column<int>(nullable: false),
+                    Id = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppException", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Errors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    Code = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Detail = table.Column<string>(nullable: true),
+                    InnerExceptionId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Errors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Errors_AppException_InnerExceptionId",
+                        column: x => x.InnerExceptionId,
+                        principalTable: "AppException",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Errors_InnerExceptionId",
+                table: "Errors",
+                column: "InnerExceptionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Errors");
+
+            migrationBuilder.DropTable(
+                name: "AppException");
+        }
+    }
+}
