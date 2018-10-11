a5127ef4344321e478d2f5d302d42b74c83c74c6
Steve Tipton <slaatipton26@gmail.com>
2018-10-11 15:01:02 -0400

    Add extension method to convert HttpRequest to HttpRequestMeta

--- scrimp/Helpers/ApiErrors.cs
+++ scrimp/Helpers/ApiErrors.cs
@@ -1,4 +1,5 @@
-﻿using scrimp.Dtos;
+﻿using Microsoft.AspNetCore.Http;
+using scrimp.Dtos;
 using scrimp.Entities;
 using System.Collections.Generic;
 
@@ -8,5 +9,15 @@ namespace scrimp.Helpers
     {
         public static ErrorList ToErrorList(this IEnumerable<Error> errors) => new ErrorList { Errors = errors };
         public static ErrorList ToErrorList(this Error error) => new ErrorList { Errors = new List<Error> { error } };
+
+        public static HttpRequestMeta ToHttpRequestMeta (this HttpRequest httpRequest)
+        {
+            return new HttpRequestMeta
+            {
+                Path = httpRequest.Path.Value,
+                PathBase = httpRequest.PathBase.Value,
+                Method = httpRequest.Method
+            };
+        }
     }
 }
