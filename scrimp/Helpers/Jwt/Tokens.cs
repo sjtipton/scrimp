125f9cc0708cb4493d665d8ad35c3575866baf5c
Steve Tipton <slaatipton26@gmail.com>
2018-10-20 08:56:30 -0400

    Add Jwt helpers and services

---
+++ scrimp/Helpers/Jwt/Tokens.cs
@@ -0,0 +1,27 @@
+﻿using scrimp.Services;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace scrimp.Helpers.Jwt
+{
+    public class Tokens
+    {
+        public static async Task<string> GenerateJwt(ClaimsIdentity identity,
+                                                     IJwtService jwtService,
+                                                     string identifier,
+                                                     JwtIssuerOptions jwtOptions,
+                                                     JsonSerializerSettings serializerSettings)
+        {
+            var response = new
+            {
+                id = identity.Claims.Single(c => c.Type == "id").Value,
+                auth_token = await jwtService.GenerateEncodedToken(identifier, identity),
+                expires_in = (int)jwtOptions.ValidFor.TotalSeconds
+            };
+
+            return JsonConvert.SerializeObject(response, serializerSettings);
+        }
+    }
+}
