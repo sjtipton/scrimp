e4dbc12b7315760bca00e97dadd6ede39f26d73d
Steve Tipton <slaatipton26@gmail.com>
2018-10-20 08:58:50 -0400

    Integrate authentication into UsersController

--- scrimp/Controllers/UsersController.cs
+++ scrimp/Controllers/UsersController.cs
@@ -1,37 +1,89 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using scrimp.Dtos;
 using scrimp.Entities;
 using scrimp.Helpers;
+using scrimp.Helpers.Jwt;
 using scrimp.Services;
 using System;
+using System.Threading.Tasks;
 
 namespace scrimp.Controllers
 {
-    // TODO add Authorization
+    [Authorize]
     [ApiController]
     [Route("api/users")]
     public class UsersController : ControllerBase
     {
         private IUserService _userService;
+        private GreenlitRestApiClient _greenlitApiClient;
+        private IJwtService _jwtService;
+        private JwtIssuerOptions _jwtOptions;
         private IErrorService _errorService;
         private IMapper _mapper;
 
-        public UsersController(IUserService userService, IErrorService errorService, IMapper mapper)
+        public UsersController(IUserService userService,
+                               GreenlitRestApiClient greenlitApiClient,
+                               IJwtService jwtService,
+                               IOptions<JwtIssuerOptions> jwtOptions,
+                               IErrorService errorService,
+                               IMapper mapper)
         {
             _userService = userService;
+            _greenlitApiClient = greenlitApiClient;
+            _jwtService = jwtService;
+            _jwtOptions = jwtOptions.Value;
             _errorService = errorService;
             _mapper = mapper;
         }
 
-        // TODO consider authenticate/authorize/register endpoints
-
-        // GET api/me
-        [HttpGet]
-        [Route("~/api/me")]
-        public IActionResult GetAuthenticatedUser()
+        // POST api/users/authenticate
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody]GreenlitAuthDto authDto)
         {
-            throw new NotImplementedException();
+            // we will already have a valid access token from Greenlit, as far as JWT is concerned
+            // we just need to find out if we have a corresponding User in Scrimp to map to --
+            // if not, we create that Scrimp User, and set the Greenlit API ID to the User
+            // to persist the mapping (i.e. so the other User details are de-coupled)
+            try
+            {
+                var user = _userService.GetByApiId(authDto.ApiId);
+
+                if (user == null)
+                {
+                    var greenlitUser = await _greenlitApiClient.GetGreenlitRestApiUser(authDto.ApiId);
+
+                    var appUser = new User
+                    {
+                        FirstName = greenlitUser.FirstName,
+                        LastName = greenlitUser.LastName,
+                        EmailAddress = greenlitUser.EmailAddress,
+                        GreenlitApiId = greenlitUser.Id
+                    };
+
+                    var result = _userService.Create(appUser);
+                }
+
+                var localUser = _userService.GetByApiId(authDto.ApiId);
+
+                if (localUser == null)
+                {
+                    return BadRequest(_errorService.BadRequest(new AppException("Failed to create local user account"), HttpContext.Request));
+                }
+
+                var jwt = await Tokens.GenerateJwt(_jwtService.GenerateClaimsIdentity(localUser.EmailAddress, localUser.Id),
+                    _jwtService, localUser.EmailAddress, _jwtOptions, new JsonSerializerSettings { Formatting = Formatting.Indented });
+
+                return new OkObjectResult(jwt);
+            }
+            catch (AppException ex)
+            {
+                return BadRequest(_errorService.BadRequest(ex, HttpContext.Request));
+            }
         }
 
         // GET api/users/:id
